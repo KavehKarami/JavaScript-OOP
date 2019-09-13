cb03915ad196b1cbe9eaad2f8d4316c3dd1dec6d
Kaveh <Kaveh.Karami.KK@gmail.com>
2019-09-13 19:39:34 +0430

    Private Properties and Methods

--- asset/js/script.js
+++ asset/js/script.js
@@ -1,3 +1,23 @@
-const circle = {
+    //Objects
+//01 - Private Properties and Methods
 
-};
+/*
+
+function Circle(radius = 1 ) {
+    this.radius = radius;
+    this.draw = ()=> "draw";
+}
+const another = new Circle(3);
+another.radius = 2;
+console.log(another);
+
+*/
+
+function Circle(radius ) {
+    let number = radius;
+    this.draw = ()=> number;
+}
+const another = new Circle(2);
+console.log(another);
+console.log(another.draw());
+
