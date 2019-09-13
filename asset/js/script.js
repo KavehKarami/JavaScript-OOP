dd53553ff7a68818369038e46decd983f15bbd04
Kaveh <Kaveh.Karami.KK@gmail.com>
2019-09-13 23:01:27 +0430

    Getters and Setters

--- asset/js/script.js
+++ asset/js/script.js
@@ -17,8 +17,44 @@ function Circle(radius = 1) {
     //be in soorat private mikonim
     let number = radius;
     this.draw = ()=> number;
+
+//02 - Getters and Setters
+    let defaultLocation = {x: 0 , y: 0};
+    // this.getDefaultLocation = function () {
+    //     return defaultLocation;
+    // };
+
+    //age khasti line 36 ta 40 ro az comment dar biari line 27 ta 33 ro comment kon ta asaresh ro bebini
+    defaultLocation = {};
+    Object.defineProperty(defaultLocation,"x", {
+        enumerable: true,
+        value: 0,
+    });
+    Object.defineProperty(defaultLocation,"y", {
+        enumerable: true,
+        value: 0,
+    });
+    Object.defineProperty(this,'defaultLocation',{
+        get(){
+            let key =Object.keys(defaultLocation);
+            let keyJoin = key.join('');
+
+            if (keyJoin ==="x")
+                return defaultLocation;
+            else
+                throw new Error('invalid key')
+        },
+        set(value) {
+            if (!value.x || !value.y)
+                throw new Error('invalid location.');
+            defaultLocation = value;
+        }
+    });
 }
+
 const another = new Circle(2);
+another.defaultLocation.y = 100 ;
 console.log(another);
-console.log(another.draw());
+console.log(another.defaultLocation);
+
 
