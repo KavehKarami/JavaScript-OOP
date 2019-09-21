1bcc070eef6c7d49fc717d34152f057675ec5adb
Kaveh <Kaveh.Karami.KK@gmail.com>
2019-09-21 14:26:12 +0430

    End of Symbol

--- 04 - ES6 Classes/asset/js/script.js
+++ 04 - ES6 Classes/asset/js/script.js
@@ -1,3 +1,4 @@
+'use strict';
 class Circle{
     constructor(radius){
         this.radius = radius;
@@ -25,8 +26,48 @@ name("Kaveh");
     //line 1 to 9
 
 //class expression
-const Square = class{constructor(radius){
+const Radius = class{constructor(radius){
     this.radius = radius
 }};
-const sq = new Square(1);
+const rd = new Radius(1);
+console.log(rd);
+
+//static method
+class Square{
+    constructor(size,radius){
+        this.size = size;
+        this.radius = radius;
+    }
+    static parse(json){
+        const size = JSON.parse(json).size;
+        const radius = JSON.parse(json).radius;
+        return new Square(size,radius)
+    }
+}
+const sq = Square.parse('{"size": 2,"radius": 3}');
 console.log(sq);
+
+const That = function () {
+    //age az arrow function estefade konim line 57 this be window bar nemigarde
+    this.draw = function(){console.log(this)}
+};
+const t = new That();
+
+const draw = t.draw;
+draw();
+
+
+const _size = Symbol();
+const _draw = Symbol();
+class SomeThing{
+    constructor(radius){
+        this[_size] = radius;
+    }
+    [_draw](){
+        console.log('hello');
+    }
+}
+const st = new SomeThing(1);
+console.log(st);
+console.log(st[_draw]());
+console.log(st[_size]);
